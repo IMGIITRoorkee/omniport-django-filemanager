b62f91d603b87d99d0856f21db4b355daaf3f6a8
gauransh7 <gauransh7dingwani@gmail.com>
2020-10-29 22:19:07 +0530

    Make single serializer/Route for file

--- serializers.py
+++ serializers.py
@@ -14,43 +14,13 @@ class FileSerializer(ModelSerializer):
     class Meta:
         model = File
         fields = '__all__'
-        read_only_fields = ['extension', 'size']
+        read_only_fields = []
 
     def get_path(self, obj):
         return obj.file_relative_path()
 
-
-class FileCreateSerializer(ModelSerializer):
-    """
-    Serializer for creating file object
-    """
-
-    class Meta:
-        model = File
-        exclude = ('folder',)
-
-    def create(self, validated_data):
-        """
-        Create and return new File, given the validated data
-        """
-
-        person = self.context['request'].person
-        folder, created = Folder.objects.get_or_create(person=person)
-        file_object = File.objects.create(**validated_data, folder=folder)
-        return file_object
-
-
-class FileUpdateSerializer(ModelSerializer):
-    """
-    Serializer for updating file object
-    """
-
-    class Meta:
-        model = File
-        fields = ('file_name', 'is_public', 'id', 'datetime_modified')
-
-
 class subFolderSerializer(ModelSerializer):
+    filemanagername = serializers.ReadOnlyField()
     class Meta:
         model = Folder
         fields = '__all__'
@@ -63,6 +33,7 @@ class FolderSerializer(ModelSerializer):
 
     files = FileSerializer(many=True, source='file_set', read_only=True)
     folders = subFolderSerializer(read_only=True, many=True)
+    filemanagername = serializers.ReadOnlyField()
 
     # def get_folders(self, obj):
     #     return obj.folders.all()
@@ -70,7 +41,7 @@ class FolderSerializer(ModelSerializer):
     class Meta:
         model = Folder
         fields = '__all__'
-        read_only_fields = ['person', 'max_space', 'content_size']
+        read_only_fields = ['person','filemanagername', 'max_space', 'content_size']
 
     def create(self, validated_data):
         """
