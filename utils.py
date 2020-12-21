cb2231977c0b9cd618bf183bb6443b753e5d2639
Ayush Bansal <ayu023ban@gmail.com>
2020-12-21 20:53:22 +0530

    resolve error cases in updating root folders and deleting migration files

--- utils.py
+++ utils.py
@@ -1,6 +1,7 @@
 from django_filemanager.models import Folder, FileManager
 from kernel.managers.get_role import get_all_roles
 
+
 def update_root_folders(person):
     for filemanager in FileManager.objects.all():
         try:
@@ -10,13 +11,13 @@ def update_root_folders(person):
             try:
                 unique_name = eval(filemanager.folder_name_template)
             except:
-                return dict({'status':400, 'message':f"{filemanager} : problem in evaluating folder name template"})
+                return dict({'status': 400, 'message': f"{filemanager} : problem in evaluating folder name template"})
             try:
                 code = compile(
                     filemanager.filemanager_access_permissions, '<bool>', 'eval')
                 filemanager_access_permission = eval(code)
             except:
-                return dict({'status':400, 'message':f"{filemanager} : problem in evaluating access permission"})
+                return dict({'status': 400, 'message': f"{filemanager} : problem in evaluating access permission"})
 
             if filemanager_access_permission:
                 try:
@@ -32,8 +33,11 @@ def update_root_folders(person):
 
                     folder = Folder.objects.get(
                         person=person, root=None, parent=None)
-                    
-                    return dict({'status':200,'message':"updated filemanagers"})
-                except:
-                    return dict({'status':400, 'message':"Unable to create root folder"})
 
+                    return dict({'status': 200, 'message': "updated filemanagers"})
+                except:
+                    return dict({'status': 400, 'message': "Unable to create root folder"})
+            else:
+                return dict({'status': 403, 'message': "not allowed"})
+    
+    return dict({'status': 200, 'message': "found filemanagers"})
