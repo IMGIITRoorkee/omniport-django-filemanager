3a4669ed2a7f0fd90e94ed37028fb1ee36cc0d4e
Ayush Bansal <ayu023ban@gmail.com>
2020-12-29 12:48:29 +0530

    make separate api for checking if the user has admin rights

--- http_urls.py
+++ http_urls.py
@@ -4,6 +4,7 @@ from django_filemanager.views.file import FileAccessView, FileView
 from django_filemanager.views.folder import FolderViewSet
 from django_filemanager.views.filemanager import FileManagerViewSet
 from django_filemanager.views.items import ItemSharedView, AllStarredItems, AllSharedItems
+from django_filemanager.views.is_admin_rights import IsAdminRights
 from rest_framework import routers
 from django.conf import settings
 from django.conf.urls.static import static
@@ -21,7 +22,8 @@ urlpatterns = [
     path(r'shared_item/<str:uuid>/<str:item1>/<int:id>/<str:item2>/',
          ItemSharedView.as_view(), name='shared_item'),
     path(r'all_shared_items/', AllSharedItems.as_view(), name='all_shared_items'),
-    path(r'all_starred_items/', AllStarredItems.as_view(), name='all_starred_items')
+    path(r'all_starred_items/', AllStarredItems.as_view(), name='all_starred_items'),
+    path(r'is_admin_rights/', IsAdminRights.as_view(), name='is_admin_rights')
 ]
 
 urlpatterns += router.urls
